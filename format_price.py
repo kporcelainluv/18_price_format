c716564560d5d92f78988f1541b66c29e3f468c3
Ksusha Zhukova <zhukovaksusha@gmail.com>
2018-02-16 19:04:41 +0300

    update logic, shorten code, use .format

--- format_price.py
+++ format_price.py
@@ -1,5 +1,4 @@
 import argparse
-import os
 
 
 def parse_command_line_args():
@@ -12,44 +11,21 @@ def parse_command_line_args():
     return parser.parse_args()
 
 
-def count_banknotes(banknotes):
-    if len(banknotes) > 3:
-        banknotes = int(float(banknotes))
-        banknotes = list(str(banknotes))[::-1]
-        price_with_whitespaces = list("".join(l + " " * (n % 3 == 2) for n, l in enumerate(banknotes)))
-        banknotes = "".join(price_with_whitespaces)[::-1]
-        if banknotes[0] == " ":
-            banknotes = banknotes[1:]
-    return banknotes
-
-
-def count_coins(price):
-    coins = os.path.splitext(str(price))[1][1:]
-    if coins is "":
-        return False
-    if all([int(coins)]) == 0:
-        return False
-    else:
-        coins_list = list(coins)
-        for digit in reversed(coins_list):
-            if digit == "0":
-                coins_list.pop()
-            else:
-                break
-        cents = "".join(coins_list)
-    return "{}{}".format(",", cents)
-
-
 def format_price(price):
-    banknotes, coins = count_banknotes(price), count_coins(price)
-    return banknotes if coins is False else "{}{}".format(banknotes, coins)
+    price = float(price)
+    if price.is_integer():
+        price = '{:,}'.format(int(price))
+        return price.replace(",", " ")
+    else:
+        price = "{0:.2f}".format(round(float(price), 2))
+        price = '{:,}'.format(float(price)).replace(",", " ").replace(".", ",")
+        return price
 
 
 if __name__ == "__main__":
     args = parse_command_line_args()
     try:
         if bool(float(args.price)):
-            banknotes, coins = count_banknotes(args.price), count_coins(args.price)
             print(format_price(args.price))
     except ValueError:
         exit("Enter a float of numeric digits")
